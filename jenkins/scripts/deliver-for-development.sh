51b17a2610ba6599581a5c500f34909c975fb8ec
iisfaq <stuff@crowe.co.nz>
2023-02-12 07:47:31 +1300

    jenkins

--- jenkins/scripts/deliver-for-development.sh
+++ jenkins/scripts/deliver-for-development.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
-
 echo 'The following "npm" command runs your Node.js/React application in'
 echo 'development mode and makes the application available for web browsing.'
 echo 'The "npm start" command has a trailing ampersand so that the command runs'
@@ -9,10 +8,8 @@ echo 'is followed by another command that retrieves the process ID (PID) value'
 echo 'of the previously run process (i.e. "npm start") and writes this value to'
 echo 'the file ".pidfile".'
 #set -x
-npm start &
-echo $! > .pidfile
+npm start & echo $! > .pidfile
 #set +x
-
 echo 'Now...'
 echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
 echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
